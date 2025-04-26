93c9ef5273339827eee9b4a550ca059a75b74cc7
Prateek Tandon <ptandon1@andrew.cmu.edu>
2025-04-26 13:20:34 -0700

    Adding lambda component and SNS

--- main.tf
+++ main.tf
@@ -80,3 +80,49 @@ resource "aws_iam_role_policy" "db_admin_policy" {
     region     = var.aws_region
   })
 }
+
+# Lambda IAM Role
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Effect = "Allow"
+      Sid    = ""
+    }]
+  })
+}
+
+# Lambda Function
+resource "aws_lambda_function" "etl_function" {
+  function_name = "ETLFunction"
+  runtime       = "python3.8"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  filename      = "path/to/your/lambda.zip"
+}
+
+# S3 Event Notification to Lambda
+resource "aws_s3_bucket_notification" "s3_event" {
+  bucket = aws_s3_bucket.etl_bucket.bucket
+
+  lambda_function {
+    events = ["s3:ObjectCreated:*", "s3:ObjectModified:*"]
+    filter_prefix = "data/"
+    lambda_function_arn = aws_lambda_function.etl_function.arn
+  }
+}
+
+# Lambda Permissions for S3 Invocation
+resource "aws_lambda_permission" "allow_s3_invocation" {
+  statement_id  = "AllowS3Invocation"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.etl_function.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.etl_bucket.arn
+}
