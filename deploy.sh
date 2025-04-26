117db5f9d995aafbc25d3ebceb9013189420bc36
Prateek Tandon <erprateek@github.com>
2025-04-26 11:37:13 -0700

    Cleaning up deployment script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo "Initializing Terraform..."
+terraform init
+
+# Validate the configuration
+echo "Validating Terraform configuration..."
+terraform validate
+
+# Generate the plan (this shows you what will be changed)
+echo "Generating Terraform plan..."
+terraform plan -out=tfplan
+
+# Apply the plan to create resources on AWS
+echo "Applying Terraform plan..."
+terraform apply tfplan
+
+echo "Deployment complete."
+
